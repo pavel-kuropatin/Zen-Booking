d8127805483e4d4f6bad66cfdbde2033349a47b4
Pavel Kuropatin <kuropatin.pv@gmail.com>
2021-05-21 02:58:45 +0300

    Add application starter classes

---
+++ src/main/java/com/kuropatin/bookingapp/BookingAppStarter.java
@@ -0,0 +1,12 @@
+package com.kuropatin.bookingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingAppStarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingAppStarter.class, args);
+    }
+}
