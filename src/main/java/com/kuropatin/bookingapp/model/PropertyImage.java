ade79794cf7fde613559655086e6b5283bc666d9
Pavel Puropatin <kuropatin.pv@gmail.com>
2021-05-22 19:35:58 +0300

    Add PropertyImage model

---
+++ src/main/java/com/kuropatin/bookingapp/model/PropertyImage.java
@@ -0,0 +1,52 @@
+package com.kuropatin.bookingapp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "property_image")
+@SQLDelete(sql = "UPDATE property_image SET deleted = true WHERE id=?")
+@Where(clause = "banned = false AND deleted = false")
+public class PropertyImage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "property_id")
+    private long propertyId;
+
+    @Column(name = "img_url")
+    private String imgUrl;
+
+    @Column(name = "approved")
+    private boolean approved;
+
+    @Column(name = "deleted")
+    private boolean deleted;
+
+    @Column(name = "created")
+    private Timestamp created;
+
+    @Column(name = "updated")
+    private Timestamp updated;
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.JSON_STYLE);
+    }
+}
