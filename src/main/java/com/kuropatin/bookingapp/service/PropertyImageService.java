7b9bf4ae92aff31aae1d7992d91f58a71ae80d25
Pavel Puropatin <kuropatin.pv@gmail.com>
2021-05-23 00:26:13 +0300

    Implement repository functionality for User, Image, PropertyImage services

--- src/main/java/com/kuropatin/bookingapp/service/PropertyImageService.java
+++ src/main/java/com/kuropatin/bookingapp/service/PropertyImageService.java
@@ -1,12 +1,13 @@
 package com.kuropatin.bookingapp.service;
 
 import com.kuropatin.bookingapp.exception.PropertyImageNotFoundException;
+import com.kuropatin.bookingapp.exception.PropertyNotFoundException;
 import com.kuropatin.bookingapp.model.PropertyImage;
 import com.kuropatin.bookingapp.repository.PropertyImageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
+import java.text.MessageFormat;
 import java.util.List;
 
 @Service
@@ -20,15 +21,15 @@ public class PropertyImageService {
     }
 
     public List<PropertyImage> getAllImagesOfProperty(Long propertyId) {
-        return repository.getAllImagesOfProperty(propertyId);
-    }
-
-    public PropertyImage getImageOfPropertyById(Long propertyId, Long imageId) {
-        return repository.getImageOfPropertyById(propertyId, imageId);
+        return repository.findAllImagesOfProperty(propertyId);
     }
 
     public PropertyImage getPropertyImageById(Long imageId) {
-        return repository.findById(imageId).orElseThrow(() -> new PropertyImageNotFoundException(imageId));
+        if(repository.existsById(imageId)) {
+            return repository.findPropertyImageById(imageId);
+        } else {
+            throw new PropertyNotFoundException(imageId);
+        }
     }
 
     public PropertyImage create(Long userId, PropertyImage propertyImage) {
@@ -36,17 +37,50 @@ public class PropertyImageService {
         return repository.save(propertyImage);
     }
 
-    @Transactional
     public PropertyImage update(Long id, PropertyImage propertyImage) {
         PropertyImage propertyImageToUpdate = getPropertyImageById(id);
-        propertyImageToUpdate.setImgUrl(propertyImage.getImgUrl());
-        return propertyImageToUpdate;
+        if(propertyImage.equals(propertyImageToUpdate)) {
+            return propertyImageToUpdate;
+        } else {
+            propertyImageToUpdate.setImgUrl(propertyImage.getImgUrl());
+            return propertyImageToUpdate;
+        }
     }
 
-    @Transactional
-    public PropertyImage deleteById(Long id) {
-        PropertyImage propertyImageToDelete = getPropertyImageById(id);
-        repository.delete(propertyImageToDelete);
-        return propertyImageToDelete;
+    public String softDeletePropertyImage(Long imageId) {
+        if(repository.existsById(imageId)) {
+            repository.softDeletePropertyImage(imageId);
+            return MessageFormat.format("Property Image with id: {0} successfully deleted", imageId);
+        } else {
+            throw new PropertyImageNotFoundException(imageId);
+        }
+    }
+
+    public List<PropertyImage> getAllNotApprovedPropertyImage(){
+        return repository.findAllNotApprovedPropertyImage();
+    }
+
+    public PropertyImage getNotApprovedPropertyImageById(Long imageId){
+        if(repository.existsById(imageId)) {
+            return repository.findNotApprovedPropertyImageById(imageId);
+        } else {
+            throw new PropertyImageNotFoundException(imageId);
+        }
+    }
+
+    public PropertyImage approvePropertyImage(Long imageId){
+        if(repository.existsById(imageId)) {
+            return repository.approvePropertyImage(imageId);
+        } else {
+            throw new PropertyImageNotFoundException(imageId);
+        }
+    }
+
+    public PropertyImage disapprovePropertyImage(Long imageId){
+        if(repository.existsById(imageId)) {
+            return repository.disapprovePropertyImage(imageId);
+        } else {
+            throw new PropertyImageNotFoundException(imageId);
+        }
     }
 }
