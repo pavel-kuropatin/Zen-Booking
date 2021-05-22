9b913963a97c7115c6a6f41ed7e588da5caf4c29
Pavel Kuropatin <kuropatin.pv@gmail.com>
2021-05-23 00:25:08 +0300

    Add custom queries for User, Image, PropertyImage repositories

--- src/main/java/com/kuropatin/bookingapp/repository/PropertyImageRepository.java
+++ src/main/java/com/kuropatin/bookingapp/repository/PropertyImageRepository.java
@@ -8,9 +8,24 @@ import java.util.List;
 
 public interface PropertyImageRepository extends CrudRepository<PropertyImage, Long> {
 
-    @Query(value = "SELECT * FROM property_image WHERE property_id = ?1", nativeQuery = true)
-    List<PropertyImage> getAllImagesOfProperty(Long propertyId);
+    @Query(value = "SELECT * FROM property_image WHERE approved = true AND deleted = false AND property_id = ?1", nativeQuery = true)
+    List<PropertyImage> findAllImagesOfProperty(Long propertyId);
 
-    @Query(value = "SELECT * FROM property_image WHERE property_id = ?1 AND id = ?2", nativeQuery = true)
-    PropertyImage getImageOfPropertyById(Long propertyId, Long imageId);
+    @Query(value = "SELECT * FROM property_image WHERE approved = true AND deleted = false AND id = ?1", nativeQuery = true)
+    PropertyImage findPropertyImageById(Long imageId);
+
+    @Query(value = "UPDATE property_image SET deleted = true WHERE id = ?1", nativeQuery = true)
+    PropertyImage softDeletePropertyImage(Long imageId);
+
+    @Query(value = "SELECT * FROM property_image WHERE approved = false AND deleted = false", nativeQuery = true)
+    List<PropertyImage> findAllNotApprovedPropertyImage();
+
+    @Query(value = "SELECT * FROM property_image WHERE approved = false AND deleted = false AND id = ?1", nativeQuery = true)
+    PropertyImage findNotApprovedPropertyImageById(Long imageId);
+
+    @Query(value = "UPDATE property_image SET approved = true WHERE id = ?1", nativeQuery = true)
+    PropertyImage approvePropertyImage(Long imageId);
+
+    @Query(value = "UPDATE property_image SET approved = false WHERE id = ?1", nativeQuery = true)
+    PropertyImage disapprovePropertyImage(Long imageId);
 }
