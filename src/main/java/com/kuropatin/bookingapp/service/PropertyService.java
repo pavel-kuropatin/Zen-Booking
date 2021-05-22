1e66553211210d2cfb60ecc4883f9c509c30c196
Pavel Kuropatin <kuropatin.pv@gmail.com>
2021-05-23 00:26:13 +0300

    Implement repository functionality for User, Image, PropertyImage services

--- src/main/java/com/kuropatin/bookingapp/service/PropertyService.java
+++ src/main/java/com/kuropatin/bookingapp/service/PropertyService.java
@@ -5,69 +5,128 @@ import com.kuropatin.bookingapp.model.Property;
 import com.kuropatin.bookingapp.repository.PropertyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
+import java.text.MessageFormat;
 import java.util.List;
 
 @Service
 public class PropertyService {
 
     private final PropertyRepository repository;
-//    private final UserService userService;
 
     @Autowired
     public PropertyService(PropertyRepository repository) {
         this.repository = repository;
-//        this.userService = userService;
     }
 
     public List<Property> getAllPropertyOfUser(Long userId) {
-        return repository.getAllPropertyOfUser(userId);
-    }
-
-    public Property getPropertyOfUserById(Long userId, Long propertyId) {
-        return repository.getPropertyOfUserById(userId,propertyId);
+        return repository.findAllPropertyOfUser(userId);
     }
 
     public Property getPropertyById(Long propertyId) {
-        return repository.findById(propertyId).orElseThrow(() -> new PropertyNotFoundException(propertyId));
+        if(repository.existsById(propertyId)) {
+            return repository.findPropertyById(propertyId);
+        } else {
+            throw new PropertyNotFoundException(propertyId);
+        }
     }
 
-    public Property create(Long userId, Property property) {
+    public Property createProperty(Long userId, Property property) {
         property.setUserId(userId);
         return repository.save(property);
     }
 
-    @Transactional
-    public Property update(Long id, Property property) {
-        Property propertyToUpdate = getPropertyById(id);
-        propertyToUpdate.setType(property.getType());
-        propertyToUpdate.setName(property.getName());
-        propertyToUpdate.setDescription(property.getDescription());
-        propertyToUpdate.setCountry(property.getCountry());
-        propertyToUpdate.setRegion(property.getRegion());
-        propertyToUpdate.setCity(property.getCity());
-        propertyToUpdate.setStreet(property.getStreet());
-        propertyToUpdate.setBuilding(property.getBuilding());
-        propertyToUpdate.setApartment(property.getApartment());
-        propertyToUpdate.setPrice(property.getPrice());
-        propertyToUpdate.setGuests(property.getGuests());
-        propertyToUpdate.setRooms(property.getRooms());
-        propertyToUpdate.setBeds(property.getBeds());
-        propertyToUpdate.setKitchen(property.isKitchen());
-        propertyToUpdate.setWasher(property.isWasher());
-        propertyToUpdate.setTv(property.isTv());
-        propertyToUpdate.setInternet(property.isInternet());
-        propertyToUpdate.setPetsAllowed(property.isPetsAllowed());
-        propertyToUpdate.setAvailable(property.isAvailable());
-        return propertyToUpdate;
+    public Property updateProperty(Long propertyId, Property property) {
+        Property propertyToUpdate = getPropertyById(propertyId);
+        if(property.equals(propertyToUpdate)) {
+            return propertyToUpdate;
+        } else {
+            propertyToUpdate.setType(property.getType());
+            propertyToUpdate.setName(property.getName());
+            propertyToUpdate.setDescription(property.getDescription());
+            propertyToUpdate.setCountry(property.getCountry());
+            propertyToUpdate.setRegion(property.getRegion());
+            propertyToUpdate.setCity(property.getCity());
+            propertyToUpdate.setStreet(property.getStreet());
+            propertyToUpdate.setBuilding(property.getBuilding());
+            propertyToUpdate.setApartment(property.getApartment());
+            propertyToUpdate.setPrice(property.getPrice());
+            propertyToUpdate.setGuests(property.getGuests());
+            propertyToUpdate.setRooms(property.getRooms());
+            propertyToUpdate.setBeds(property.getBeds());
+            propertyToUpdate.setKitchen(property.isKitchen());
+            propertyToUpdate.setWasher(property.isWasher());
+            propertyToUpdate.setTv(property.isTv());
+            propertyToUpdate.setInternet(property.isInternet());
+            propertyToUpdate.setPetsAllowed(property.isPetsAllowed());
+            propertyToUpdate.setAvailable(property.isAvailable());
+            return repository.save(propertyToUpdate);
+        }
     }
 
-    @Transactional
-    public Property deleteById(Long id) {
-        Property propertyToDelete = getPropertyById(id);
-        repository.delete(propertyToDelete);
-        return propertyToDelete;
+    public String softDeleteProperty(Long propertyId) {
+        if(repository.existsById(propertyId)) {
+            repository.softDeleteProperty(propertyId);
+            return MessageFormat.format("Property with id: {0} successfully deleted", propertyId);
+        } else {
+            throw new PropertyNotFoundException(propertyId);
+        }
+    }
+
+    public List<Property> getAllNotApprovedProperty(){
+        return repository.findAllNotApprovedProperty();
+    }
+
+    public Property getNotApprovedPropertyById(Long propertyId){
+        if(repository.existsById(propertyId)) {
+            return repository.findNotApprovedPropertyById(propertyId);
+        } else {
+            throw new PropertyNotFoundException(propertyId);
+        }
+    }
+
+    public List<Property> getAllBannedProperty(){
+        return repository.findAllBannedProperty();
+    }
+
+    public Property getBannedPropertyById(Long propertyId){
+        if(repository.existsById(propertyId)) {
+            return repository.findBannedPropertyById(propertyId);
+        } else {
+            throw new PropertyNotFoundException(propertyId);
+        }
+    }
+
+    public Property approveProperty(Long propertyId){
+        if(repository.existsById(propertyId)) {
+            return repository.approveProperty(propertyId);
+        } else {
+            throw new PropertyNotFoundException(propertyId);
+        }
+    }
+
+    public Property disapproveProperty(Long propertyId){
+        if(repository.existsById(propertyId)) {
+            return repository.disapproveProperty(propertyId);
+        } else {
+            throw new PropertyNotFoundException(propertyId);
+        }
+    }
+
+    public Property banProperty(Long propertyId) {
+        if(repository.existsById(propertyId)) {
+            return repository.banProperty(propertyId);
+        } else {
+            throw new PropertyNotFoundException(propertyId);
+        }
+    }
+
+    public Property unbanProperty(Long propertyId) {
+        if(repository.existsById(propertyId)) {
+            return repository.unbanProperty(propertyId);
+        } else {
+            throw new PropertyNotFoundException(propertyId);
+        }
     }
 
 //    @Transactional
--- src/main/java/com/kuropatin/bookingapp/service/UserService.java
+++ src/main/java/com/kuropatin/bookingapp/service/UserService.java
@@ -5,11 +5,9 @@ import com.kuropatin.bookingapp.model.User;
 import com.kuropatin.bookingapp.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
+import java.text.MessageFormat;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
 
 @Repository
 public class UserService {
@@ -22,33 +20,71 @@ public class UserService {
     }
 
     public List<User> getAllUsers() {
-        return StreamSupport.stream(repository.findAll().spliterator(), false).collect(Collectors.toList());
+        return repository.findAllUsers();
     }
 
     public User getUserById(Long id) {
-        return repository.findById(id).orElseThrow(() -> new UserNotFoundException(id));
+        if(repository.existsById(id)) {
+            return repository.findUserById(id);
+        } else {
+            throw new UserNotFoundException(id);
+        }
     }
 
-    public User create(User user) {
+    public User createUser(User user) {
         return repository.save(user);
     }
 
-    public User update(Long id, User user) {
+    public User updateUser(Long id, User user) {
         User userToUpdate = getUserById(id);
-        userToUpdate.setLogin(user.getLogin());
-        userToUpdate.setPassword(user.getPassword());
-        userToUpdate.setName(user.getName());
-        userToUpdate.setSurname(user.getSurname());
-        userToUpdate.setBirthDate(user.getBirthDate());
-        userToUpdate.setEmail(user.getEmail());
-        userToUpdate.setPhone(user.getPhone());
-        return userToUpdate;
+        if(user.equals(userToUpdate)) {
+            return userToUpdate;
+        } else {
+            userToUpdate.setLogin(user.getLogin());
+            userToUpdate.setPassword(user.getPassword());
+            userToUpdate.setName(user.getName());
+            userToUpdate.setSurname(user.getSurname());
+            userToUpdate.setBirthDate(user.getBirthDate());
+            userToUpdate.setEmail(user.getEmail());
+            userToUpdate.setPhone(user.getPhone());
+            return repository.save(userToUpdate);
+        }
     }
 
-    @Transactional
-    public User deleteById(Long id) {
-        User userToDelete = getUserById(id);
-        repository.delete(userToDelete);
-        return userToDelete;
+    public String softDeleteUser(Long id) {
+        if(repository.existsById(id)) {
+            repository.softDeleteUser(id);
+            return MessageFormat.format("User with id: {0} successfully deleted", id);
+        } else {
+            throw new UserNotFoundException(id);
+        }
+    }
+
+    public List<User> findAllBannedUsers(){
+        return repository.findAllBannedUsers();
+    }
+
+    public User findBannedUserById(Long id){
+        if(repository.existsById(id)) {
+            return repository.findBannedUserById(id);
+        } else {
+            throw new UserNotFoundException(id);
+        }
+    }
+
+    public User banUser(Long id) {
+        if(repository.existsById(id)) {
+            return repository.banUser(id);
+        } else {
+            throw new UserNotFoundException(id);
+        }
+    }
+
+    public User unbanUser(Long id) {
+        if(repository.existsById(id)) {
+            return repository.unbanUser(id);
+        } else {
+            throw new UserNotFoundException(id);
+        }
     }
 }
