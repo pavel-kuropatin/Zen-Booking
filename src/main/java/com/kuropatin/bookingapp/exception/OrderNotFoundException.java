de235140b43a27d800feb1741624521e7b0614c8
Pavel Puropatin <kuropatin.pv@gmail.com>
2021-05-22 19:24:40 +0300

    Add PropertyImage, Order, Review not found exceptions. Correct ";" to ":" in the exception message

---
+++ src/main/java/com/kuropatin/bookingapp/exception/OrderNotFoundException.java
@@ -0,0 +1,10 @@
+package com.kuropatin.bookingapp.exception;
+
+import java.text.MessageFormat;
+
+public class OrderNotFoundException extends RuntimeException {
+
+    public OrderNotFoundException(Long id) {
+        super(MessageFormat.format("Could not find order with id: {0}", id));
+    }
+}
