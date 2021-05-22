df272255d8328d7b3477c6ddb4690898d07ea4c0
Pavel Kuropatin <kuropatin.pv@gmail.com>
2021-05-22 19:26:21 +0300

    Add PropertyImage, Order, Admin, Review repositories

---
+++ src/main/java/com/kuropatin/bookingapp/repository/AdminRepository.java
@@ -0,0 +1,8 @@
+package com.kuropatin.bookingapp.repository;
+
+import com.kuropatin.bookingapp.model.Admin;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AdminRepository extends CrudRepository<Admin, Long> {
+
+}
---
+++ src/main/java/com/kuropatin/bookingapp/repository/PropertyImageRepository.java
@@ -0,0 +1,8 @@
+package com.kuropatin.bookingapp.repository;
+
+import com.kuropatin.bookingapp.model.Property;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PropertyImageRepository extends CrudRepository<Property, Long> {
+
+}
