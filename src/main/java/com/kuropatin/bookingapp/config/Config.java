5a978c84a910aaf27b97a1df3c5ba559a47142b0
Pavel Kuropatin <kuropatin.pv@gmail.com>
2021-05-23 21:23:54 +0300

    Add Swagger-UI config

--- src/main/java/com/kuropatin/bookingapp/config/Config.java
+++ src/main/java/com/kuropatin/bookingapp/config/Config.java
@@ -1,11 +1,31 @@
 package com.kuropatin.bookingapp.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.function.Predicate;
 
 @Configuration
 @EnableSwagger2
 public class Config {
 
+    @Bean
+    public Docket docket() {
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select()
+                .apis(Predicate.not(RequestHandlerSelectors.basePackage("org.springframework.boot")))
+                .build();
+    }
 
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("Booking Application API")
+                .description("This API can be used to create a booking application")
+                .version("v0.0.1-SNAPSHOT")
+                .build();
+    }
 }
