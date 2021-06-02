4007aa348dc20a79e1b3c3afd0004154e2706a7e
Pavel Kuropatin <kuropatin.pv@gmail.com>
2021-06-02 15:44:34 +0300

    Add Order repository

--- src/main/java/com/kuropatin/bookingapp/repository/OrderRepository.java
+++ src/main/java/com/kuropatin/bookingapp/repository/OrderRepository.java
@@ -1,8 +1,34 @@
 package com.kuropatin.bookingapp.repository;
 
 import com.kuropatin.bookingapp.model.Order;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import javax.transaction.Transactional;
+import java.util.List;
 
 public interface OrderRepository extends CrudRepository<Order, Long> {
 
+    @Query(value = "SELECT * FROM orders WHERE accepted_by_host = true AND cancelled = false AND property_id = ?1", nativeQuery = true)
+    List<Order> findAllOrders(Long propertyId);
+
+    @Query(value = "SELECT * FROM orders WHERE accepted_by_host = true AND cancelled = false AND id = ?1", nativeQuery = true)
+    Order findOrderById(Long orderId);
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE orders SET cancelled = true WHERE id = ?1", nativeQuery = true)
+    void cancelOrder(Long orderId);
+
+    @Query(value = "SELECT * FROM orders WHERE accepted_by_host = false AND cancelled = false", nativeQuery = true)
+    List<Order> findAllNotApprovedPOrders();
+
+    @Query(value = "SELECT * FROM orders WHERE accepted_by_host = false AND cancelled = false AND id = ?1", nativeQuery = true)
+    Order findNotApprovedOrdersById(Long orderId);
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE orders SET accepted_by_host = true WHERE id = ?1", nativeQuery = true)
+    Order acceptOrder(Long orderId);
 }
