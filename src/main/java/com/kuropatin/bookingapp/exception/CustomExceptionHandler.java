2e28a5f8deaa357eaf0f833daeaed0785eaf4931
Pavel Puropatin <kuropatin.pv@gmail.com>
2021-06-19 02:55:38 +0300

    Edit exception handler to remove duplicate code

--- src/main/java/com/kuropatin/bookingapp/exception/CustomExceptionHandler.java
+++ src/main/java/com/kuropatin/bookingapp/exception/CustomExceptionHandler.java
@@ -15,56 +15,44 @@ class CustomExceptionHandler {
 
     @ExceptionHandler(UserNotFoundException.class)
     protected ResponseEntity<Object> userNotFoundHandler(UserNotFoundException e) {
-        return throwCustomNotFoundException(e);
+        return throwCustomException(e, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(PropertyNotFoundException.class)
     protected ResponseEntity<Object> propertyNotFoundHandler(PropertyNotFoundException e) {
-        return throwCustomNotFoundException(e);
+        return throwCustomException(e, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(PropertyImageNotFoundException.class)
     protected ResponseEntity<Object> propertyImageNotFoundHandler(PropertyImageNotFoundException e) {
-        return throwCustomNotFoundException(e);
+        return throwCustomException(e, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(OrderNotFoundException.class)
     protected ResponseEntity<Object> orderNotFoundHandler(OrderNotFoundException e) {
-        return throwCustomNotFoundException(e);
+        return throwCustomException(e, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(ReviewNotFoundException.class)
     protected ResponseEntity<Object> reviewNotFoundHandler(ReviewNotFoundException e) {
-        return throwCustomNotFoundException(e);
+        return throwCustomException(e, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(InsufficientMoneyAmountException.class)
     protected ResponseEntity<Object> insufficientMoneyAmountHandler(InsufficientMoneyAmountException e) {
-        Map<String, Object> body = new LinkedHashMap<>();
-        body.put("timestamp", LocalDateTime.now());
-        body.put("status", HttpStatus.PAYMENT_REQUIRED.value());
-        body.put("error", e.getMessage());
-        return new ResponseEntity<>(body, HttpStatus.PAYMENT_REQUIRED);
+        return throwCustomException(e, HttpStatus.PAYMENT_REQUIRED);
     }
 
     @ExceptionHandler(SQLException.class)
-    protected ResponseEntity<Object> reviewNotFoundHandler(SQLException e) {
-        return throwCustomSQLException(e);
+    protected ResponseEntity<Object> sqlExceptionHandler(SQLException e) {
+        return throwCustomException(e, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    private ResponseEntity<Object> throwCustomNotFoundException(RuntimeException e) {
+    private ResponseEntity<Object> throwCustomException(Exception e, HttpStatus httpStatus) {
         Map<String, Object> body = new LinkedHashMap<>();
         body.put("timestamp", LocalDateTime.now());
-        body.put("status", HttpStatus.NOT_FOUND.value());
+        body.put("status", httpStatus.value());
         body.put("error", e.getMessage());
-        return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
-    }
-
-    private ResponseEntity<Object> throwCustomSQLException(Exception e) {
-        Map<String, Object> body = new LinkedHashMap<>();
-        body.put("timestamp", LocalDateTime.now());
-        body.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
-        body.put("error", e.getMessage());
-        return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(body, httpStatus);
     }
 }
