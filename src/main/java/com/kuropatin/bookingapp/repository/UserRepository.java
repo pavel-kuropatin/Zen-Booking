ae1dd01e92408fe37fb0bc6738721b154c392f97
Pavel Puropatin <kuropatin.pv@gmail.com>
2021-06-23 21:12:50 +0300

    Repositories now use spring @Transactional

--- src/main/java/com/kuropatin/bookingapp/repository/UserRepository.java
+++ src/main/java/com/kuropatin/bookingapp/repository/UserRepository.java
@@ -4,8 +4,8 @@ import com.kuropatin.bookingapp.model.User;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 import java.util.List;
 
 public interface UserRepository extends CrudRepository<User, Long> {
