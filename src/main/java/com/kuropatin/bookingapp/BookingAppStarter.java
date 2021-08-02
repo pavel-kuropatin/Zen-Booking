b4822c5567b83639a2081b1c2f72061ba456821f
Pavel Kuropatin <kuropatin.pv@gmail.com>
2021-08-02 15:24:35 +0300

    Prepare to deploy to Heroku

--- src/main/java/com/kuropatin/bookingapp/BookingAppStarter.java
+++ src/main/java/com/kuropatin/bookingapp/BookingAppStarter.java
@@ -3,7 +3,7 @@ package com.kuropatin.bookingapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.kuropatin.bookingapp")
 public class BookingAppStarter {
 
     public static void main(String[] args) {
