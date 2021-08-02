943d04e0b7e793de337e4d1cbe41f16c126ac517
Pavel Kuropatin <kuropatin.pv@gmail.com>
2021-08-02 20:00:22 +0300

    Update SwaggerConfig. Edit application.properties. Deploy on Heroku

--- src/main/java/com/kuropatin/bookingapp/config/SwaggerConfig.java
+++ src/main/java/com/kuropatin/bookingapp/config/SwaggerConfig.java
@@ -3,6 +3,7 @@ package com.kuropatin.bookingapp.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
@@ -16,16 +17,22 @@ public class SwaggerConfig {
     @Bean
     public Docket docket() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .useDefaultResponseMessages(false)
                 .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.kuropatin.bookingapp"))
+                .paths(PathSelectors.any())
                 .build();
     }
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title("Booking Application API")
-                .description("Application API documentation")
+                .description("Application API documentation.\n" +
+                        "DISCLAIMER: " +
+                        "Data security is not guaranteed. " +
+                        "When using the application, do not enter any real personal information. " +
+                        "You enter any personal information at your own risk.")
                 .version("v0.1.0-SNAPSHOT")
                 .build();
     }
