88f3fc65a68780b9743a5579e41dd87271a4dfc5
Pavel Kuropatin <kuropatin.pv@gmail.com>
2021-08-10 15:14:12 +0300

    Improve SQL query readability

--- src/main/java/com/kuropatin/bookingapp/repository/PropertyRepository.java
+++ src/main/java/com/kuropatin/bookingapp/repository/PropertyRepository.java
@@ -19,39 +19,50 @@ public interface PropertyRepository extends CrudRepository<Property, Long> {
 
     @Cacheable(value = CacheNames.BOOLEAN, key = "'existsPropertyByIdAndUserId'+#propertyId+#userId")
     @Query(value = "SELECT CASE WHEN COUNT(p.id) > 0 THEN TRUE ELSE FALSE END " +
-                   "FROM Property p WHERE p.isDeleted = false AND p.id = ?1 AND p.user.id = ?2")
+                   "FROM Property p " +
+                   "WHERE p.isDeleted = false AND p.id = ?1 AND p.user.id = ?2")
     boolean existsByIdAndUserId(Long propertyId, Long userId);
 
     @Cacheable(value = CacheNames.BOOLEAN, key = "'existsPropertyByIdAndNotUserId'+#propertyId+#userId")
     @Query(value = "SELECT CASE WHEN COUNT(p.id) > 0 THEN TRUE ELSE FALSE END " +
-                   "FROM Property p WHERE p.isDeleted = false AND p.id = ?1 AND p.user.id <> ?2")
+                   "FROM Property p " +
+                   "WHERE p.isDeleted = false AND p.id = ?1 AND p.user.id <> ?2")
     boolean existsByIdAndNotUserId(Long propertyId, Long userId);
 
     @Query(value = "SELECT CASE WHEN COUNT(p.id) > 0 THEN TRUE ELSE FALSE END " +
-            "FROM Property p " +
-            "WHERE p.id NOT IN (SELECT DISTINCT o.property.id FROM Order o " +
-            "WHERE o.isFinished = false " +
-            "AND (?1 BETWEEN o.startDate AND o.endDate " +
-            "OR ?2 BETWEEN o.startDate AND o.endDate " +
-            "OR o.startDate BETWEEN ?1 AND ?2 " +
-            "OR o.endDate BETWEEN ?1 AND ?2))"
-    )
+                   "FROM Property p " +
+                   "WHERE p.id NOT IN (SELECT DISTINCT o.property.id " +
+                                      "FROM Order o " +
+                                      "WHERE o.isFinished = false " +
+                                      "AND (?1 BETWEEN o.startDate AND o.endDate " +
+                                           "OR ?2 BETWEEN o.startDate AND o.endDate " +
+                                           "OR o.startDate BETWEEN ?1 AND ?2 " +
+                                           "OR o.endDate BETWEEN ?1 AND ?2))")
     boolean canPropertyBeOrdered(LocalDate startDate, LocalDate endDate);
 
     @Cacheable(value = CacheNames.BOOLEAN, key = "'findAllPropertyOfUser'+#userId")
-    @Query(value = "SELECT p FROM Property p WHERE p.isDeleted = false AND p.user.id = ?1 ORDER BY p.id")
+    @Query(value = "SELECT p " +
+                   "FROM Property p " +
+                   "WHERE p.isDeleted = false AND p.user.id = ?1 " +
+                   "ORDER BY p.id")
     List<Property> findAllPropertyOfUser(Long userId);
 
     @Cacheable(value = CacheNames.PROPERTY, key = "'existsByIdAndNotUserId'+#propertyId+#userId")
-    @Query(value = "SELECT p FROM Property p WHERE p.isDeleted = false AND p.id = ?1 AND p.user.id = ?2")
+    @Query(value = "SELECT p " +
+                   "FROM Property p " +
+                   "WHERE p.isDeleted = false AND p.id = ?1 AND p.user.id = ?2")
     Property findPropertyByIdAndOwnerId(Long propertyId, Long userId);
 
     @Cacheable(value = CacheNames.PROPERTY, key = "'findPropertyByIdAndNotOwnerId'+#propertyId+#userId")
-    @Query(value = "SELECT p FROM Property p WHERE p.isDeleted = false AND p.id = ?1 AND p.user.id <> ?2")
+    @Query(value = "SELECT p " +
+                   "FROM Property p " +
+                   "WHERE p.isDeleted = false AND p.id = ?1 AND p.user.id <> ?2")
     Property findPropertyByIdAndNotOwnerId(Long propertyId, Long userId);
 
     @Modifying
     @Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.DEFAULT, rollbackFor = SQLException.class)
-    @Query(value = "UPDATE Property p SET p.isAvailable = false, p.isDeleted = true, p.updated = ?2 WHERE p.id = ?1")
+    @Query(value = "UPDATE Property p " +
+                   "SET p.isAvailable = false, p.isDeleted = true, p.updated = ?2 " +
+                   "WHERE p.id = ?1")
     void softDeleteProperty(Long propertyId, Timestamp updated);
 }
