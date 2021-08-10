53dd0e153f06233fdf9fdff66402689dd3d60c1d
Pavel Puropatin <kuropatin.pv@gmail.com>
2021-08-10 15:14:12 +0300

    Improve SQL query readability

--- src/main/java/com/kuropatin/bookingapp/repository/ReviewRepository.java
+++ src/main/java/com/kuropatin/bookingapp/repository/ReviewRepository.java
@@ -18,7 +18,8 @@ public interface ReviewRepository extends CrudRepository<Review, Long> {
 
     @Cacheable(value = CacheNames.BOOLEAN, key = "'existsReviewByIdAndUserId'+#reviewId+#userId")
     @Query(value = "SELECT CASE WHEN COUNT(p.id) > 0 THEN TRUE ELSE FALSE END " +
-                   "FROM Property p WHERE p.isDeleted = false AND p.id = ?1 AND p.user.id = ?2")
+                   "FROM Property p " +
+                   "WHERE p.isDeleted = false AND p.id = ?1 AND p.user.id = ?2")
     boolean existsReviewByIdAndUserId(Long reviewId, Long userId);
 
     @Query(value = "SELECT CASE WHEN COUNT(o.id) > 0 THEN TRUE ELSE FALSE END " +
@@ -27,11 +28,16 @@ public interface ReviewRepository extends CrudRepository<Review, Long> {
     boolean canReviewBeAdded(Long orderId, Long userId);
 
     @Cacheable(value = CacheNames.REVIEW, key = "'findAllReviewsOfUser'+#userId")
-    @Query(value = "SELECT r FROM Review r WHERE r.isDeleted = false AND r.order.user.id = ?1 ORDER BY r.id DESC")
+    @Query(value = "SELECT r " +
+                   "FROM Review r " +
+                   "WHERE r.isDeleted = false AND r.order.user.id = ?1 " +
+                   "ORDER BY r.id DESC")
     List<Review> findAllReviewsOfUser(Long userId);
 
     @Cacheable(value = CacheNames.REVIEW, key = "'findReviewOfUserById'+#reviewId+#userId")
-    @Query(value = "SELECT r FROM Review r WHERE r.isDeleted = false AND r.id = ?1 AND r.order.user.id = ?2 ORDER BY r.id DESC")
+    @Query(value = "SELECT r " +
+                   "FROM Review r " +
+                   "WHERE r.isDeleted = false AND r.id = ?1 AND r.order.user.id = ?2 ORDER BY r.id DESC")
     Review findReviewOfUserById(Long reviewId, Long userId);
 
     @Cacheable(value = CacheNames.REVIEW, key = "'findReviewById'+#reviewId")
@@ -39,6 +45,8 @@ public interface ReviewRepository extends CrudRepository<Review, Long> {
 
     @Modifying
     @Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.DEFAULT, rollbackFor = SQLException.class)
-    @Query(value = "UPDATE Review r SET r.isDeleted = true, r.updated = ?2 WHERE r.id = ?1")
+    @Query(value = "UPDATE Review r " +
+                   "SET r.isDeleted = true, r.updated = ?2 " +
+                   "WHERE r.id = ?1")
     void softDeleteReview(Long reviewId, Timestamp updated);
 }
--- src/main/java/com/kuropatin/bookingapp/repository/UserRepository.java
+++ src/main/java/com/kuropatin/bookingapp/repository/UserRepository.java
@@ -26,26 +26,33 @@ public interface UserRepository extends CrudRepository<User, Long> {
 
     @Cacheable(value = CacheNames.BOOLEAN, key = "'isLoginInUse'+#login")
     @Query(value = "SELECT CASE WHEN COUNT(u.login) > 0 THEN TRUE ELSE FALSE END " +
-                   "FROM User u WHERE u.login = ?1")
+                   "FROM User u " +
+                   "WHERE u.login = ?1")
     boolean isLoginInUse(String login);
 
     @Cacheable(value = CacheNames.BOOLEAN, key = "'isEmailInUse'+#email")
     @Query(value = "SELECT CASE WHEN COUNT(u.email) > 0 THEN TRUE ELSE FALSE END " +
-                   "FROM User u WHERE u.email = ?1")
+                   "FROM User u " +
+                   "WHERE u.email = ?1")
     boolean isEmailInUse(String email);
 
     @Cacheable(value = CacheNames.BOOLEAN, key = "'isUserBanned'+#id")
     @Query(value = "SELECT CASE WHEN COUNT(u.id) > 0 THEN TRUE ELSE FALSE END " +
-                   "FROM User u WHERE u.isBanned = true AND u.id = ?1")
+                   "FROM User u " +
+                   "WHERE u.isBanned = true AND u.id = ?1")
     boolean isBanned(long id);
 
     @Modifying
     @Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.DEFAULT, rollbackFor = SQLException.class)
-    @Query(value = "UPDATE User u SET u.isBanned = true, u.updated = ?2 WHERE u.id = ?1")
+    @Query(value = "UPDATE User u " +
+                   "SET u.isBanned = true, u.updated = ?2 " +
+                   "WHERE u.id = ?1")
     User banUser(Long id, Timestamp updated);
 
     @Modifying
     @Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.DEFAULT, rollbackFor = SQLException.class)
-    @Query(value = "UPDATE User u SET u.isBanned = false, u.updated = ?2 WHERE u.id = ?1")
+    @Query(value = "UPDATE User u " +
+                   "SET u.isBanned = false, u.updated = ?2 " +
+                   "WHERE u.id = ?1")
     User unbanUser(Long id, Timestamp updated);
 }
