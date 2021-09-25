03823b83eceede60a765cd215e5d4d080b36e064
Pavel Kuropatin <kuropatin.pv@gmail.com>
2021-09-25 22:46:23 +0300

    Set access level to protected

--- src/main/java/com/kuropatin/zenbooking/model/request/AmountRequest.java
+++ src/main/java/com/kuropatin/zenbooking/model/request/AmountRequest.java
@@ -1,13 +1,13 @@
 package com.kuropatin.zenbooking.model.request;
 
 import com.kuropatin.zenbooking.validation.IntegerInRange;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @NoArgsConstructor
-@AllArgsConstructor
 @Getter
+@Setter
 public class AmountRequest {
 
     @IntegerInRange(min = 1, max = 1000, message = "Enter amount to deposit, should be between 1 and 1000")
