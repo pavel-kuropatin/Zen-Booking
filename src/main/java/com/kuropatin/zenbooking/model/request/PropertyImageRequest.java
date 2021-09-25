ab2b2d503c63da7410df175d7829a78ad5ba0bb1
Pavel Kuropatin <kuropatin.pv@gmail.com>
2021-09-25 22:47:48 +0300

    Refactor requests

--- src/main/java/com/kuropatin/zenbooking/model/request/PropertyImageRequest.java
+++ src/main/java/com/kuropatin/zenbooking/model/request/PropertyImageRequest.java
@@ -1,15 +1,15 @@
 package com.kuropatin.zenbooking.model.request;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 @NoArgsConstructor
-@AllArgsConstructor
 @Getter
+@Setter
 public class PropertyImageRequest {
 
     @NotBlank(message = "Enter image URL")
