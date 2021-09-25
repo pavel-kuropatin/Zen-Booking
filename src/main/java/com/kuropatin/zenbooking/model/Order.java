81d896eb71712117cd7b1db45cb50e318fc7b082
Pavel Puropatin <kuropatin.pv@gmail.com>
2021-09-25 21:40:04 +0300

    Create BasicEntity for models

--- src/main/java/com/kuropatin/zenbooking/model/Order.java
+++ src/main/java/com/kuropatin/zenbooking/model/Order.java
@@ -15,15 +15,11 @@ import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
-import java.sql.Timestamp;
 import java.time.LocalDate;
 import java.util.Set;
 
@@ -36,12 +32,7 @@ import java.util.Set;
 })
 @Entity
 @Table(name = "orders")
-public class Order {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
-    private long id;
+public class Order extends BasicEntity {
 
     @Column(name = "total_price")
     private int totalPrice;
@@ -60,12 +51,6 @@ public class Order {
 
     @Column(name = "is_finished")
     private boolean isFinished = false;
-
-    @Column(name = "created")
-    private Timestamp created;
-
-    @Column(name = "updated")
-    private Timestamp updated;
 
     @OneToOne
     @JoinColumn(name = "property_id")
