ac64164b96395174d2ab75f6959b1343bb3eab4a
Pavel Kuropatin <kuropatin.pv@gmail.com>
2021-09-25 20:44:46 +0300

    Move constructor to proper place

--- src/main/java/com/kuropatin/zenbooking/model/request/UserCreateRequest.java
+++ src/main/java/com/kuropatin/zenbooking/model/request/UserCreateRequest.java
@@ -10,20 +10,6 @@ import javax.validation.constraints.Size;
 @Getter
 public class UserCreateRequest extends UserEditRequest {
 
-    public UserCreateRequest(String login,
-                             String password,
-                             String name,
-                             String surname,
-                             String gender,
-                             String birthDate,
-                             String email,
-                             String phone
-    ) {
-        super(name, surname, gender, birthDate, email, phone);
-        this.login = login;
-        this.password = password;
-    }
-
     @NotBlank(message = "Enter login")
     @Size(min = 2, max = 20, message = "Login should be between 2 and 20 characters")
     private String login;
@@ -31,4 +17,19 @@ public class UserCreateRequest extends UserEditRequest {
     @NotBlank(message = "Enter password")
     @Size(min = 8, max = 20, message = "Password should be between 8 and 20 characters")
     private String password;
+
+    public UserCreateRequest(
+            String login,
+            String password,
+            String name,
+            String surname,
+            String gender,
+            String birthDate,
+            String email,
+            String phone
+    ) {
+        super(name, surname, gender, birthDate, email, phone);
+        this.login = login;
+        this.password = password;
+    }
 }
